633a3dc82cee9424fad0b6b16214656c937fbc3b
younesda <youneshachami9@gmail.com>
2025-09-09 19:42:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import io
 
